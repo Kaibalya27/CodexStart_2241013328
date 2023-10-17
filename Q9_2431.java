98cea8eb4e12c4c46c84265252764785ff0cd83a
Kaibalya27 <128764530+Kaibalya27@users.noreply.github.com>
2023-10-17 21:35:02 +0530

    Create Q9_2431.java

---
+++ Q9_2431.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Q9_2431 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+        int q = Integer.parseInt(br.readLine());
+        for (int i = 0; i < q; i++) {
+            long k = Long.parseLong(br.readLine().trim());
+            long start = 1, len = 1, curr = 9;
+
+            while (k > len * curr) {
+                k -= len * curr;
+                start *= 10;
+                len++;
+                curr *= 10;
+            }
+
+            long number = start + (k - 1) / len;
+            int digit = Long.toString(number).charAt((int) ((k - 1) % len)) - '0';
+            out.println(digit);
+        }
+
+        out.flush();
+    }
+}
