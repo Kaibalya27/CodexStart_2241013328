b3036303f92365d786c040637c82aed9ffcdc9cc
Kaibalya27 <128764530+Kaibalya27@users.noreply.github.com>
2023-10-17 18:47:31 +0530

    Create Q5_1617.java

---
+++ Q5_1617.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q5_1617 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextInt();
+        long result=1;
+        for (int i = 0; i < n; i++) {
+            result = (result * 2) % 1000000007;
+        }
+
+        System.out.println(result);
+    }
+}
